ec49574d73ff0ba0ba225fc97666ed506c6fac77
utkarshsingh03 <92692056+utkarshsingh03@users.noreply.github.com>
2023-08-11 19:43:23 +0530

    Update bubble_sort.js

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,6 +1,5 @@
 function Bubble()
 {
-    //Setting Time complexities
     document.getElementById("Time_Worst").innerText="O(N^2)";
     document.getElementById("Time_Average").innerText="Θ(N^2)";
     document.getElementById("Time_Best").innerText="Ω(N)";
